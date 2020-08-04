f574907bae9e0318aeb0538e69b7232c0e571b96
sald19 <carl.sald19@gmail.com>
2020-08-04 16:41:57 -0600

    add folder trello

---
+++ trello/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <canvas />
+</template>
+
+<script>
+import Canvas from "./components/Canvas/index.vue";
+
+export default {
+  name: "App",
+  components: {
+    Canvas,
+  },
+};
+</script>
